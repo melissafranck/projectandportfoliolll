9b90cacbb0a936dcd1ef606ef56f541511e3e94e
franckm <melissa_franck@icloud.com>
2019-07-10 03:43:10 -0400

    ajax to load json data and onload function

--- basic/js/blog.js
+++ basic/js/blog.js
@@ -1,1 +1,7 @@
-document.querySelector("#blog h2").innerHTML = "New Text";
+//document.querySelector("#blog h2").innerHTML = "New Text";
+
+//ajax to load json file
+let xhr = new XMLHttpRequest();
+xhr.onload = function(){
+    
+}
