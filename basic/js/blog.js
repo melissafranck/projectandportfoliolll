c886c7bc7f5b0b35a16d2e6c04a23b8a296941a9
franckm <melissa_franck@icloud.com>
2019-07-10 04:01:10 -0400

    inserted the html articles

--- basic/js/blog.js
+++ basic/js/blog.js
@@ -4,13 +4,12 @@
 let xhr = new XMLHttpRequest();
 xhr.onload = function(){
 //save json data
-    let data = JSON.parse(xhr.responseText);
+let data = JSON.parse(xhr.responseText);
+   
+//insert the html articles
+let articleSection = document.querySelector('#blog');   
+    
 }
-
-
-
-
-
-
 //call ajax
 xhr.open('GET', 'https://melissafranck.github.io/mindfuldata.json', true)
+xhr.send(null);
