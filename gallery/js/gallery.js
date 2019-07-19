9405e56d879ae74835601a00c21fb88a07dd9031
franckm <melissa_franck@icloud.com>
2019-07-18 21:20:41 -0400

    start of image change fuction

---
+++ gallery/js/gallery.js
@@ -0,0 +1,10 @@
+//changing the image
+function changeImage(event){
+    
+    //setting variables
+    let image = event.target.src;
+    let parent = event.target.parentElement;
+    let largeImage = document.querySelector('#gallery img');
+    
+    
+}
