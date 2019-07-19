fd394b90d1549e0015b05fb138a3c0ec97f5a97d
franckm <melissa_franck@icloud.com>
2019-07-18 21:26:55 -0400

    completed image change function

--- gallery/js/gallery.js
+++ gallery/js/gallery.js
@@ -6,5 +6,10 @@ function changeImage(event){
     let parent = event.target.parentElement;
     let largeImage = document.querySelector('#gallery img');
     
+    //change image
+    largeImage.src = image;
     
+    //changing active indicator
+    document.querySelector("#gallery .active").classList.remove("active");
+    parent.className = 'active';
 }
