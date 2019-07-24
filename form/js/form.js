5b31db8856d9e56b99a176722818874781116674
franckm <melissa_franck@icloud.com>
2019-07-24 16:17:51 -0400

    added conditional in validation function

--- form/js/form.js
+++ form/js/form.js
@@ -3,4 +3,13 @@ function validateForm(event){
       let target = event.target;
       let outter = target.parentElement;
       let error = '<lable class="error">Required!';
+    
+    //conditional for validation
+    if(!target.value.length){
+        if(!outter.querySelector('.error')){
+            parent.insertAdjacentHTML('beforeend', error);
+        }
+    }else{
+        outter.removeChild(outter.querySelector('.error'));
+    }
 }
