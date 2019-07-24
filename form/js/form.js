219b4271e50aaffc32f201d78dc633f3305ec3fa
franckm <melissa_franck@icloud.com>
2019-07-24 12:11:35 -0400

    created validation fuction and variables

---
+++ form/js/form.js
@@ -0,0 +1,6 @@
+//validation fuction
+function validateForm(event){
+      let target = event.target;
+      let outter = target.parentElement;
+      let error = '<lable class="error">Required!';
+}
